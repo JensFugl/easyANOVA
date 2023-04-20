5e69afa01d0f977cc7d0b3ea1975c161d65b2a13
JensFugl <36481404+JensFugl@users.noreply.github.com>
2023-04-20 19:21:04 +0200

    Inital commit

---
+++ easyANOVA.py
@@ -0,0 +1,155 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+from scipy import stats
+import seaborn as sns
+import matplotlib.pyplot as plt
+from io import BytesIO
+import base64
+
+
+st.set_page_config(
+    page_title="ANOVA Analysis and Violin Plot Web App",
+    page_icon=":bar_chart:",
+    layout="wide",
+    initial_sidebar_state="expanded",
+)
+
+
+def plot_to_base64(plt):
+    buffer = BytesIO()
+    plt.savefig(buffer, format='png')
+    buffer.seek(0)
+    img_base64 = base64.b64encode(buffer.getvalue()).decode('utf-8')
+    return img_base64
+
+
+def main_app():
+    st.title("Easy ANOVA")
+
+    uploaded_file = st.file_uploader("Upload a CSV file", type=["csv"])
+
+    if uploaded_file:
+        df = pd.read_csv(uploaded_file)
+
+        st.write("The first 5 rows of the dataset:")
+        st.write(df.head())
+
+        group_col = st.selectbox("Select the column for grouping (categorical):", df.columns)
+        value_col = st.selectbox("Select the column for values (numerical):", df.columns)
+
+        if st.button("Perform ANOVA analysis"):
+            groups = df[group_col].unique()
+            data = [df[df[group_col] == g][value_col].values for g in groups]
+            f_stat, p_value = stats.f_oneway(*data)
+
+            st.write(f"F-statistic: {f_stat}")
+            st.write(f"P-value: {p_value}")
+
+            alpha = 0.05
+            if p_value < alpha:
+                st.write("There is a statistically significant difference between the groups.")
+            else:
+                st.write("There is no statistically significant difference between the groups.")
+
+        if st.button("Plot violin plot"):
+            plt.figure(figsize=(12, 6))
+            sns.violinplot(x=group_col, y=value_col, data=df, inner="quartile")
+            plt.title("Violin Plot")
+            st.pyplot(plt.gcf())
+
+            img_base64 = plot_to_base64(plt)
+            st.download_button(
+                label="Download Violin Plot",
+                data=BytesIO(base64.b64decode(img_base64)),
+                file_name="violin_plot.png",
+                mime="image/png",
+            )
+
+
+def how_it_works():
+    st.title("How it works")
+
+    st.write("""
+    The app takes a CSV file with two columns: one for categorical data (grouping) and one for numerical data (values).
+    The app performs an ANOVA analysis to determine if there's a statistically significant difference between the groups.
+    The data is also visualized using a violin plot.
+    """)
+
+    st.write("Here's an example using the following dummy data:")
+    dummy_data = pd.DataFrame({"Group": ["A"] * 5 + ["B"] * 5 + ["C"] * 5,
+                               "Value": [10, 12, 9, 11, 13, 20, 21, 19, 18, 22, 30, 29, 31, 28, 32]})
+    
+    if st.button("Load Dummy Data"):
+        st.write(dummy_data)
+    
+    st.write("The categorical column (Group) has three groups: A, B, and C.")
+    st.write("The numerical column (Value) contains the values associated with each group.")
+
+    if len(dummy_data.columns) > 2:
+        group_col = st.selectbox("Select the column for grouping (categorical):", dummy_data.columns)
+        value_col = st.selectbox("Select the column for values (numerical):", dummy_data.columns)
+    else:
+        group_col, value_col = dummy_data.columns
+
+    if st.button("Perform ANOVA analysis for dummy data"):
+        # Perform ANOVA analysis for the dummy data
+        groups = dummy_data[group_col].unique()
+        data = [dummy_data[dummy_data[group_col] == g][value_col].values for g in groups]
+        f_stat, p_value = stats.f_oneway(*data)
+
+        st.session_state.f_stat = f_stat
+        st.session_state.p_value = p_value
+
+        alpha = 0.05
+        if p_value < alpha:
+            st.session_state.result = "There is a statistically significant difference between the groups."
+        else:
+            st.session_state.result = "There is no statistically significant difference between the groups."
+        if "f_stat" in st.session_state:
+        	st.write(f"F-statistic: {st.session_state.f_stat}")
+        	st.write(f"P-value: {st.session_state.p_value}")
+        	st.write(st.session_state.result)
+
+        # Plot the violin plot for the dummy data
+        plt.figure(figsize=(12, 6))
+        sns.violinplot(x=group_col, y=value_col, data=dummy_data, inner="quartile")
+        plt.title("Violin Plot")
+        st.pyplot(plt.gcf())
+
+        img_base64 = plot_to_base64(plt)
+        st.download_button(
+            label="Download Violin Plot",
+            data=BytesIO(base64.b64decode(img_base64)),
+            file_name="violin_plot_dummy_data.png",
+            mime="image/png",
+        )
+
+
+
+
+
+
+
+
+
+# Create a navigation menu with the main app and the how_it_works page
+pages = {
+    "Easy ANOVA": main_app,
+    "How it Works": how_it_works,
+}
+
+st.sidebar.title("")
+page = st.sidebar.radio("", list(pages.keys()))
+
+# Display the selected page
+pages[page]()
+
+hide_st_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            header {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_st_style, unsafe_allow_html=True)
