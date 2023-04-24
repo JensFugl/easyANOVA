16d1e885332100c1628b6f76cb4ae78fd234ed64
JensFugl <36481404+JensFugl@users.noreply.github.com>
2023-04-24 21:29:36 +0200

    Debugging app page3

--- pages/page3.py
+++ pages/page3.py
@@ -7,7 +7,7 @@ import plotly.io as pio
 from io import BytesIO
 import base64
 
-def plot_to_base64(fig):
+def convert_fig_to_base64(fig):
     img_base64 = base64.b64encode(pio.to_image(fig, format="png")).decode("utf-8")
     return img_base64
 
@@ -20,15 +20,14 @@ def app():
         data = pd.read_csv(uploaded_file)
 
         st.write(data)
-        group_col = st.selectbox("Select the column for grouping (categorical):", [col.strip() for col in data.columns])
-        value_col = st.selectbox("Select the column for values (numerical):", [col.strip() for col in data.columns])
-
+        group_col = st.selectbox("Select the column for grouping (categorical):", data.columns.str.strip())
+        value_col = st.selectbox("Select the column for values (numerical):", data.columns.str.strip())
 
         if st.button("Perform ANOVA analysis"):
             # Perform ANOVA analysis for the uploaded data
             groups = data[group_col].unique()
-            data = [data[data[group_col] == g][value_col].values for g in groups]
-            f_stat, p_value = stats.f_oneway(*data)
+            anova_data = [data[data[group_col] == g][value_col].values for g in groups]
+            f_stat, p_value = stats.f_oneway(*anova_data)
 
             st.session_state.f_stat = f_stat
             st.session_state.p_value = p_value
@@ -45,10 +44,10 @@ def app():
                 st.markdown(st.session_state.result, unsafe_allow_html=True)
 
             # Plot the violin plot for the uploaded data
-            fig_data = px.violin(data_frame=data, x=str(group_col), y=str(value_col), box=True, points="all", title="Violin Plot")
+            fig_data = px.violin(data_frame=data, x=group_col, y=value_col, box=True, points="all", title="Violin Plot")
             fig_data.update_layout(autosize=True, margin=dict(l=0, r=0, t=30, b=0), width=None)
             st.plotly_chart(fig_data, use_container_width=True)
-            img_base64_data = plot_to_base64(fig_data)
+            img_base64_data = convert_fig_to_base64(fig_data)
             st.download_button(
                 label="Download Violin Plot",
                 data=BytesIO(base64.b64decode(img_base64_data)),
